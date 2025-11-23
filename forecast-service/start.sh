c3fe61a2e9b3cd923ed08fe700670a7ac58cf749
wizardsWeb <voravinay82@gmail.com>
2025-11-23 11:15:26 +0530

    pull

--- forecast-service/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start the forecast service
-echo "Starting Forecast Service..."
-echo "Make sure you have installed dependencies: pip install -r requirements.txt"
-echo ""
-
-python app.py
-
